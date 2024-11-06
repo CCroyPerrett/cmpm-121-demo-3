1eea96d7f3c20d2e8cba94394f4a7bddb0f0c8e4
EldritchSalamancer <147461070+CCroyPerrett@users.noreply.github.com>
2024-11-06 02:40:13 -0800

    button

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,7 @@
 // todo
+const alert_button = document.createElement("button");
+document.body.append(alert_button);
+alert_button.innerHTML = "alert";
+alert_button.addEventListener("click", () => {
+  alert("you clicked the button!");
+});
