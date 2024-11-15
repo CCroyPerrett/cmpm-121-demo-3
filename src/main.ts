f0e3db24e21f968f6517c0813a923f6e1e66ef43
EldritchSalamancer <147461070+CCroyPerrett@users.noreply.github.com>
2024-11-14 17:44:25 -0800

    reset

--- src/main.ts
+++ src/main.ts
@@ -13,31 +13,11 @@ import "./leafletWorkaround.ts";
 import luck from "./luck.ts";
 //L.imageOverlay();
 
-// todo
-/*const alert_button = document.createElement("button");
-document.body.append(alert_button);
-alert_button.innerHTML = "alert";
-alert_button.addEventListener("click", () => {
-  alert("you clicked the button!");
-});
-*/
 const mapdiv = document.createElement("div"); mapdiv.id = "map";
 document.body.append(mapdiv);
 
 let points:number[] = [36.98949379578401, -122.06277128548504];
 const playerpos = leaflet.latLng(points[0], points[1]);
-//const mapdata: MapOptions {
- // center: new L.LatLng(40.731253, -73.996139),
- // zoom: 12
-//};
-/*const map = leaflet.map(document.getElementById("map")!, {
-  center: OAKES_CLASSROOM,
-  zoom: GAMEPLAY_ZOOM_LEVEL,
-  minZoom: GAMEPLAY_ZOOM_LEVEL,
-  maxZoom: GAMEPLAY_ZOOM_LEVEL,
-  zoomControl: false,
-  scrollWheelZoom: false,
-});*/
 
 const gamemap = leaflet.map(document.getElementById("map")!,{
   zoom:19, center: playerpos,
@@ -52,14 +32,14 @@ leaflet
   })
   .addTo(gamemap);
 
+const playerMarker = leaflet.marker(playerpos);
+playerMarker.bindTooltip("There you are! Get to geocaching!");
+playerMarker.addTo(gamemap);
+
 function reloadmap(){
   gamemap.panTo(new leaflet.LatLng(points[0], points[1]))
   playerMarker.setLatLng(new leaflet.LatLng(points[0], points[1]));
 }
-
-const playerMarker = leaflet.marker(playerpos);
-playerMarker.bindTooltip("There you are! Get to geocaching!");
-playerMarker.addTo(gamemap);
 
 const up = document.createElement("button"); 
 up.innerHTML = "↑"; document.body.append(up);
@@ -86,5 +66,13 @@ const right = document.createElement("button");
 right.innerHTML = "→"; document.body.append(right);
 right.addEventListener('click', (event) => {
   points[1] += 0.0001;
+  reloadmap();
+});
+
+const reset = document.createElement("button"); 
+reset.innerHTML = "reset"; document.body.append(reset);
+reset.addEventListener('click', (event) => {
+  points[1] += 0.0001;
+  points = [36.98949379578401, -122.06277128548504];
   reloadmap();
 });
